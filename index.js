4f114baebbc3fb174528aac5526f3e431edcf0f8
KohakuChan <69802296+MelidaZ@users.noreply.github.com>
2022-01-18 01:38:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 const discord = require("discord.js");
+const Voice = require("@discordjs/voice");
 const client = new discord.Client();
 const ytdl = require('ytdl-core');
-const LINK = '#Youtube Live link';
-const Channel = '#Your Channel ID';
-const Server = '#Your Server ID';
+const MusicLink = '#Youtube Live link';
+const MusicChannelID = '#Your Channel ID';
+const ServerID = '#Your Server ID';
 const TOKEN = '#Your Bot Token';
 
-client.on('ready', async () => {
-  let channel = client.channels.cache.get(Channel) || await client.channels.fetch(Channel)
-  if(!channel) return console.log('Not Found Channel to Join')
-  const connection = await channel.join();
-  const dispenser =  connection.play(ytdl(LINK), { highWaterMark: 1 << 150, quality:'highestaudio', filter:'audioonly'})
-  dispenser.setVolume(0.5)
-})
+/*----------------------------------------------------- FUNCTIONS -----------------------------------------------------*/
+async function Music247() {
+    const guild = client.guilds.cache.get(ServerID);
+    const MusicChannel = guild.channels.cache.get(MusicChannelID);
+    const roomone = Voice.joinVoiceChannel({
+        channelId: MusicChannelID,
+        guildId: ServerID,
+        adapterCreator: MusicChannel.guild.voiceAdapterCreator
+    });
+    const player = Voice.createAudioPlayer({
+        behaviors: {
+            noSubscriber: Voice.NoSubscriberBehavior.Play,
+            maxMissedFrames: Math.round(2000 / 20),
+        }
+    });
+    // noinspection JSCheckFunctionSignatures
+    const resource = await Voice.createAudioResource(ytdl(MusicLink), {
+        inlineVolume: true,
+        highWaterMark: 1 << 25, // 64 MB
+        seek: Math.floor(-1800 / 2)
+    });
+    resource.volume.setVolume(0.075);
+    await player.play(resource);
+    if (!Voice.getVoiceConnections(ServerID)) {
+        roomone.subscribe(player);
+    }
+}
 
-setInterval(async function() {
-  if(!client.voice.connections.get(Server)) {
-    let channel = client.channels.cache.get(Channel) || await client.channels.fetch(Channel)
-    if(!channel) return console.log('Not Found Channel to Join')
-    const connection = await channel.join();
-    const dispenser = connection.play(ytdl(LINK), { highWaterMark: 1 << 150, quality:'highestaudio', filter:'audioonly'})
-    dispenser.setVolume(0.5)
-  }
-}, 10000)
+/*----------------------------------------------------- FUNCTIONS -----------------------------------------------------*/
+
+client.on('ready', async () => {
+    await Music247();
+    setInterval(async () => {
+        await Music247();
+    }, 10000);
+});
 
 client.login(TOKEN)
